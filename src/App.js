f737ab05d79c93aac1c523f65c88fb5b78007ad6
ktaeeun43 <ktaeeun43@gmail.com>
2022-08-09 13:39:14 +0900

    bitvelo side project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>사이드 프로젝트 BitSide</h1>
+    </div>
+  );
+}
+
+export default App;
