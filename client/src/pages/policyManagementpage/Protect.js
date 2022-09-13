abfd9dff3707b68df76d3a572d4b1621957173ed
ktaeeun43 <ktaeeun43@gmail.com>
2022-09-13 16:06:15 +0900

    다운로드 버튼 구현

--- client/src/pages/policyManagementpage/Protect.js
+++ client/src/pages/policyManagementpage/Protect.js
@@ -4,6 +4,26 @@ import { Outlet } from "react-router-dom";
 import PolicyManagementLayout from "../../templates/PolicyManagementLayout";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
+import { COLOR_LAYOUT_BACKGROUND, COLOR_WHITE, COLOR_ABLE_BUTTON } from "../../constants";
+
+const Button = styled.div`
+
+  width: 10%;
+  height: 20%;
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  justify-content: center;
+  padding: 0.3rem 0.875rem;
+  color: ${COLOR_WHITE};
+  font-size: 0.875rem;
+  background-color: ${COLOR_ABLE_BUTTON};
+  cursor: pointer;
+  border-radius: 4px;
+  word-break: keep-all;
+  margin-top: 20px;
+`;
 
 const StyledTableCellValue = styled.div`
 display: flex;
@@ -35,13 +55,18 @@ const Protect = () => {
   }, []);
   console.log(assets,"자산")
   console.log(user.userData.isAdmin,"자산")
-  return (
+
+  
+
+    return (
     <>
       <Outlet />
       <PolicyManagementLayout>
         <h1>정보보호정책</h1>
         {assets.map((asset, idx) => {
-              let asset_name = asset.filePath.split('policy')[1];
+              let asset_name2 = asset.filePath.split('policy')[1];
+              let asset_name3 = asset_name2.split('.')[0];
+              let asset_name = asset_name3.substr(1)
               return (
                 <>
                  <StyledTableRow key={asset._id}>
@@ -49,10 +74,10 @@ const Protect = () => {
                       <StyledTableCellValue>{idx}</StyledTableCellValue>
                       <StyledTableCellValue>{asset.title}</StyledTableCellValue>
                       <StyledTableCellValue>{asset.description}</StyledTableCellValue>
+                      <StyledTableCellValue>{asset_name}</StyledTableCellValue>
                       {user.userData.isAdmin === true ? (
-                      <StyledTableCellValue><a href={`http://bitside.net:3000/${asset.filePath}`}>{asset_name}</a></StyledTableCellValue>
-                      ) : (
-                        <StyledTableCellValue>{asset_name}</StyledTableCellValue>
+                        <Button><a href={`http://bitside.net:3000/${asset.filePath}`} style={{textDecoration: 'none', color: "white"}}>다운로드</a></Button>
+                      ) : ( null
                   )}
                     </TableInCell>
                   </StyledTableRow>
