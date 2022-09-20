08a1cef0c868fb46b2e0049ec8c89052191e803c
Changraekang <storkcr@gmail.com>
2022-09-20 14:27:32 +0900

    css 수정

--- client/src/pages/documentManagement/Receipt.js
+++ client/src/pages/documentManagement/Receipt.js
@@ -6,91 +6,26 @@ import axios from "axios";
 import moment from "moment";
 
 
-const Wrapper = styled.div`
-height: ${(props) => (props.isMobile ? "100%" : "")};
-display: flex;
-flex-direction: column;
-background-color: white;
-border: 1px solid rgb(230, 230, 230);
-padding: ${(props) => (props.isMobile ? "1.1875rem" : "40px 20px")};
-overflow: auto;
-`;
-
-const TitleWrapper = styled.div`
- display: flex;
- display: -webkit-box;
- -webkit-box-orient: vertical;
- justify-content: center;
- align-items: center;
- flex-direction: column;
-`;
-
-const BoxTitle = styled.div`
-  width: 70%;
-  padding-bottom: ${(props) => (props.isMobile ? "10px" : "18px")};
-  display: flex;
-  justify-content: center;
-  font-size: 23px;
-  color: #02215e;
-`;
-
-const RegisterInputWrapper = styled.div`
-justify-content: center;
-
-`;
-
-const InputLabel = styled.label`
-  margin-left: 20px;
-  font-size: 12px;
-  color: #727272;
-  margin: 0px;
-`;
-
-const Input = styled.input`
-  margin-top: 10px;
-  font-size: 12px;
-  color: #033e93;
-  height: 30px;
-  border: 1px solid rgb(200, 200, 200);
-  padding-left: 10px;
-  padding-right: 10px;
-`;
-
-const Button = styled.div`
-
-  width: 300px;
-  display: flex;
-  -webkit-box-align: center;
-  align-items: center;
-  -webkit-box-pack: center;
-  justify-content: center;
-  padding: 0.3rem 0.875rem;
-  color: ${COLOR_WHITE};
-  font-size: 0.875rem;
-  background-color: ${COLOR_ABLE_BUTTON};
-  cursor: pointer;
-  border-radius: 4px;
-  word-break: keep-all;
-`;
-
 const BoxWrapper = styled.div`
  flex: 1 0 0%;
  max-width: 1000px;
  width: 100%;
  margin: ${(props) => (props.isMobile ? "0 auto" : "20px auto")};
  position: relative;
-`;
+ `;
+ 
+ const InputWrapper = styled.div`
+ display: flex;
+ width: 100%;
+ justify-content: space-around;
+ `;
+ 
+ const InBoxWrapper = styled.div`
+ display:flex;
+ width: 100%;
+justify-content: space-around;
+background-color: #f5f5f7;
 
-const InputWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 20px;
-`;
-
-const InBoxWrapper = styled.div`
-padding: 30px;
-display:flex;
-justify-content: center;
 `;
 
 
@@ -119,15 +54,27 @@ const StyledTableCellTitle = styled.div`
   align-items: center;
   background-color: #f5f5f7;
   flex: 0 0 4.8rem;
-  padding: 1rem 2.2rem;
   word-break: keep-all;
+  
+  overflow: auto;
 `;
 const StyledTableCellValue = styled.div`
 display: flex;
+width: 100%;
+height: 200px;
 align-items: center;
 flex: 0 0 4.8rem;
-padding: 1rem 2.2rem;
-  word-break: keep-all;
+
+word-break: keep-all;
+overflow: auto;
+font-size: 6px;
+`;
+const StyledTableCellOperateValue = styled.div`
+display: flex;
+width:10%;
+align-items: center;
+justify-content: center;
+flex: 0 0 4.8rem;
 `;
 
 const TableInCell = styled.td`
@@ -146,7 +93,6 @@ const Receipt = () => {
       }
     });
   }, []);
-  console.log(receipts,"명세서")
 
   return (
     <>
@@ -155,27 +101,29 @@ const Receipt = () => {
         <Title></Title>
         </Table>
         <StyledTableRow>
-          <TableCell>
-            <StyledTableCellTitle>번호</StyledTableCellTitle>
+          <InBoxWrapper>
+            <StyledTableCellOperateValue>번호</StyledTableCellOperateValue>
             <StyledTableCellTitle>통제항목</StyledTableCellTitle>
             <StyledTableCellTitle>상세내용</StyledTableCellTitle>
-            <StyledTableCellTitle>운영여부</StyledTableCellTitle>
+            <StyledTableCellOperateValue>운영여부</StyledTableCellOperateValue>
             <StyledTableCellTitle>운영현황 (또는 미선택사유)</StyledTableCellTitle>
             <StyledTableCellTitle>관련문서 (정책,지침 등 세부조항번호까지)</StyledTableCellTitle>
             <StyledTableCellTitle>기록 (증적자료)</StyledTableCellTitle>
-          </TableCell>
+          </InBoxWrapper>
         </StyledTableRow>
         {receipts.map((receipt, idx) => {
               return (
                 <>
                   <StyledTableRow key={document._id}>
-                    <TableInCell>
-                      <StyledTableCellValue>{idx}</StyledTableCellValue>
+                    <InputWrapper>
+                      <StyledTableCellOperateValue>{idx}</StyledTableCellOperateValue>
                       <StyledTableCellValue>{receipt.controlItem}</StyledTableCellValue>
                       <StyledTableCellValue>{receipt.content}</StyledTableCellValue>
-                      <StyledTableCellValue>{receipt.operation}</StyledTableCellValue>
+                      <StyledTableCellOperateValue>{receipt.operation}</StyledTableCellOperateValue>
                       <StyledTableCellValue>{receipt.status}</StyledTableCellValue>
-                    </TableInCell>
+                      <StyledTableCellValue>{receipt.status}</StyledTableCellValue>
+                      <StyledTableCellValue>{receipt.status}</StyledTableCellValue>
+                    </InputWrapper>
                   </StyledTableRow>
                 </>
               );
